aa8449c315e0a29ba13e4013c252e77029932299
pixie-dev <gayathriperumal100@gmail.com>
2020-12-13 23:26:01 +0530

    Fix media screen alignment

--- components/Home.js
+++ components/Home.js
@@ -3,10 +3,10 @@ import styles from '../styles/Home.module.css'
 
 const Home = () => {
   return (
-    <>
+    <div className={styles.home}>
       <h1 className={styles.title}>Hello Welcome to my Virtual Home</h1>
-     
-    </>
+      <p className={styles.subtitle}>Checkout my new articles below</p>     
+    </div>
   )
 }
 
